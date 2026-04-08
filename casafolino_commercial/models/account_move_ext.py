436e4616fc876ee035278f95ef811ede90d347b5
folinofood <folinofood.com>
2026-04-08 22:08:55 +0200

    feat: bottone blocchi documento + lista inline

--- casafolino_commercial/models/account_move_ext.py
+++ casafolino_commercial/models/account_move_ext.py
@@ -11,3 +11,22 @@ class AccountMoveExt(models.Model):
         'block_id',
         string='Blocchi Documento',
     )
+    footer_block_count = fields.Integer(
+        compute='_compute_footer_block_count',
+        string='Blocchi',
+    )
+
+    def _compute_footer_block_count(self):
+        for rec in self:
+            rec.footer_block_count = len(rec.footer_block_ids)
+
+    def action_open_footer_blocks(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Blocchi Documento',
+            'res_model': 'cf.doc.footer.block',
+            'view_mode': 'list,form',
+            'domain': [('id', 'in', self.footer_block_ids.ids)],
+            'context': {'default_active': True},
+            'target': 'current',
+        }
