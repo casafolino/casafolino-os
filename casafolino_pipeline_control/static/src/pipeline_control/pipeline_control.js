3f5729f0fcc5262b335bc3036d6d04c199351dd4
folinofood <folinofood.com>
2026-05-22 06:51:32 +0200

    Default control room to dossier view

--- casafolino_pipeline_control/static/src/pipeline_control/pipeline_control.js
+++ casafolino_pipeline_control/static/src/pipeline_control/pipeline_control.js
@@ -16,7 +16,7 @@ export class CFPipelineControl extends Component {
         this.state = useState({
             loading: true,
             error: null,
-            activeView: this.props.action?.context?.default_view || "control",
+            activeView: this.props.action?.context?.default_view || "dossiers",
             selectedFairId: false,
             inboxFilter: "all",
             dossierSearch: "",
